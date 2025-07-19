8fb1073f5bfaa9baea9e2258afb1e058c5015ca2
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-07-19 13:17:33 +0530

    Access Lists using simple method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 
 function App() {
-  const ecosystem_projects = ["Aave", "Uniswap", "Lido", "ENS"];
+  const ecosystem_projects = [
+    {name : "Aave", description: "Lending"},
+    {name : "ENS", description: "Domain provider"}
+  ];
   return (
     <div className="App">
-    {ecosystem_projects.map((project, key) => {
-      return <h2 key={key}> {project} </h2>;
-    })}
-    </div>
-  );
-}
-
-// Components
-const Ethereum = (props) => {
-  return (
-    // jsx
-    <div>
-      <h1> {props.name} </h1> 
-      <h2> {props.description} </h2>
-      <h3> {props.age} </h3>
+      {ecosystem_projects.map((project, key) => {
+        return <h2> {project.name} {project.description}</h2>;
+      })}
     </div>
   );
 }
