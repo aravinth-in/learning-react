bf0bd179c81ebfef89b6ab642057e87f754aec7e
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-07-19 12:02:31 +0530

    Components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Ethereum />
+    </div>
+  );
+}
+
+// Components
+const Ethereum = () => {
+  return (
+    // jsx
+    <div>
+      <h1> Ethereum </h1> 
+      <h2> World Ledger</h2>
+      <h3>10 years</h3>
+    </div>
+  );
+}
+
+export default App;
