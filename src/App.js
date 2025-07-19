b9ae8533dbe41cb917c7ace624f385df803fa4ab
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-07-19 13:35:59 +0530

    Increment count not updated in UI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import {Project} from './Project.js'
 
 function App() {
-  const ecosystem_projects = [
-    {name : "Aave", description: "Lending"},
-    {name : "ENS", description: "Domain provider"}
-  ];
+  let count = 0;
+
+  // This will increase the count, but not in UI as react render this count only once.
+  // refer count_increment_not_updated_ui.png
+  const increaseCount = () => {
+    count++;
+    console.log(count);
+  }
   return (
     <div className="App">
-      {ecosystem_projects.map((project, key) => {
-        return <Project name={project.name} des={project.description} />;
-      })}
+      {count} <button onClick={increaseCount}> Increase Count</button>
     </div>
   );
 }
