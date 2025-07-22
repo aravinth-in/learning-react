11620d4ddd54d4691405f435662c16c1ff303b1d
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-07-22 10:32:57 +0530

    Use custom hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import { useState } from "react";
+import { useToggle } from './useToggle';
 
 function App() {
-  const [isVisible, setIsVisible] = useState(false);
+  // These two toggle are completedly independent of each other .
+  const [isVisible, toggle] = useToggle();
+  // UseToggle returns [state, toggle] you can set the custom name as you like while using
+  // it. for eg. isVisible is the name of the state here.
+  // const {state, toggle} = useToggle(); - in this case you can't set the custom name.
+  // other way const {state: isVisible, toggle } = useToggle();
+  const [isVisible2, toggle2] = useToggle();
 
   return (
     <div className="App">
-      <button onClick={() => setIsVisible((prev) => !prev)}> {isVisible? "Hide" : "Show"} </button>
+      <button onClick={toggle}> {isVisible? "Hide" : "Show"} </button>
       {isVisible && <h1> Hidden text </h1>}
+
+      <button onClick={toggle2}> {isVisible2? "Hide" : "Show"} </button>
+      {isVisible2 && <h1> Hidden text </h1>}
     </div>
   );
 }
